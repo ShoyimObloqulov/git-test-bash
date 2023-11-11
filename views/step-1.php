c83c31cd6a7ab30f2fd8b51b1287facd0b7d01d2
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2023-11-11 19:07:15 +0500

    first project install github

---
+++ views/step-1.php
@@ -0,0 +1,43 @@
+<div class="fieldset-content">
+    <div class="form-group">
+        <label for="username" class="form-label">Iltimos familyangizni kiriting!</label>
+        <input type="text" name="last_name" id="username" placeholder="Familya" />
+    </div>
+
+    <div class="form-group">
+        <label for="username" class="form-label">Ismingizni kiriting!</label>
+        <input type="text" name="first_name" id="username" placeholder="Ism" />
+    </div>
+
+    <div class="form-group">
+        <label for="username" class="form-label"> Sharifingizni kiriting!</label>
+        <input type="text" name="other_name" id="username" placeholder="Sharifingiz" />
+    </div>
+    <div class="form-group">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email" id="email" placeholder="Your Email" />
+    </div>
+    <div class="form-group form-password">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" id="password" data-indicator="pwindicator" />
+        <div id="pwindicator">
+            <div class="bar-strength">
+                <div class="bar-process">
+                    <div class="bar"></div>
+                </div>
+            </div>
+            <div class="label"></div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="your_avatar" class="form-label">Select avatar</label>
+        <div class="form-file">
+            <input type="file" name="your_avatar" id="your_avatar" class="custom-file-input" />
+            <span id="val"></span>
+            <span id="button">Select File</span>
+        </div>
+    </div>
+</div>
+<div class="fieldset-footer">
+    <span>Step 1 of 3</span>
+</div>
